fafa9e759939a98b7a56b3d74e8422686709dde1
AppianZ <zhang4baichi@sina.com>
2016-06-20 13:45:39 +0800

    fix:express index

--- myapp/myapp/routes/index.js
+++ myapp/myapp/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
-var router = express.Router();
-
+var router = express();
+var admin = express();
+var secret = express();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express123' });
 });
+
+admin.get('/', function (req, res) {
+  console.log(admin.mountpath); // [ '/adm*n', '/manager' ]
+  res.send('Admin Homepage');
+});
+
+secret.get('/', function (req, res) {
+  console.log(secret.mountpath); // /secr*t
+  res.send('Admin Secret');
+});
+
+admin.use('/secr*t', secret);
+router.use(['/adm*n', '/manager'], admin);
 
 module.exports = router;
