0d0c4fc32facb3792552b399264e202ff52949b1
AppianZ <zhang4baichi@sina.com>
2016-06-29 20:47:40 +0800

    add:temp

--- myapp/myapp/routes/index.js
+++ myapp/myapp/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var router = express();
+
+var  router = express.Router();
+var app = express();
 var admin = express();
 var secret = express();
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index',
       { title: 'my app 123',
         content: 'GET request to homepage'});
@@ -20,27 +22,68 @@ secret.get('/', function (req, res) {
 });
 
 admin.use('/secr*t', secret);
-router.use(['/adm*n', '/manager'], admin);
-module.exports = router;
+app.use(['/adm*n', '/manager'], admin);
+
+router.param(function(param, option) {
+  return function (req, res, next, val) {
+    if (val == option) {
+      next();
+    }
+    else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+// using the customized router.param()
+router.param('id', 1337);
+
+// route to trigger the capture
+router.get('/user/:id', function (req, res) {
+  res.send('OK');
+});
+
+app.use(router);
+
+app.listen(3000, function () {
+  console.log('Ready');
+});
+
+
+
+
+
+
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
 
 
 // ----------------------------------------------------
-router.disable('parameter');
-console.log('1.1 ' + router.get('parameter'));//false
+app.disable('parameter');
+console.log('1.1 ' + app.get('parameter'));//false
 
-router.disabled('parameter');
-console.log('2.1 ' + router.disabled('parameter'));//true
-console.log('2.2 ' + router.get('parameter'));//false
+app.disabled('parameter');
+console.log('2.1 ' + app.disabled('parameter'));//true
+console.log('2.2 ' + app.get('parameter'));//false
 
-router.enable('parameter');
-console.log('3.1 ' + router.get('parameter'));//true
+app.enable('parameter');
+console.log('3.1 ' + app.get('parameter'));//true
 
-router.enabled('parameter');
-console.log('4.1 ' + router.enabled('parameter'));//true
-console.log('4.2 ' + router.get('parameter'));//true
+app.enabled('parameter');
+console.log('4.1 ' + app.enabled('parameter'));//true
+console.log('4.2 ' + app.get('parameter'));//true
 
-router.set('title', 'My Express App');
-console.log('5.1 ' + router.get('title'));//My Express App
+app.set('title', 'My Express App');
+console.log('5.1 ' + app.get('title'));//My Express App
 
 // ----------------------------------------------------
 
