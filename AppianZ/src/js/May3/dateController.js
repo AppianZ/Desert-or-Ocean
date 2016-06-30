b6f5c8bf70d5d275833fd35c3b99bd4ee8f7ceaf
AppianZ <zhang4baichi@sina.com>
2016-06-30 09:19:58 +0800

    add:temp

--- AppianZ/src/js/May3/dateController.js
+++ AppianZ/src/js/May3/dateController.js
@@ -1,3 +1,27 @@
 /**
  * Created by JW on 2016/6/29.
  */
+function DateSelector(config){
+
+}
+
+DateSelector.prototype = {
+    constructor : DateSelector,
+    init : function(){
+
+    }
+};
+
+new DateSelector({
+    el : "",
+    type : 0,//0：日期
+    count : 100,
+    format : 0,
+    cancel : function(){
+
+    },
+    submit : function(){
+
+    }
+});
+
